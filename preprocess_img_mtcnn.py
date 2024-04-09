5771650e2507a188827c62ba75922ed7d8600418
ante <antetc@cs.wisc.edu>
2024-04-08 22:42:23 -0500

    updated preprocessing script

--- preprocess_img_mtcnn.py
+++ preprocess_img_mtcnn.py
@@ -15,8 +15,8 @@ mtcnn = MTCNN(image_size=160, margin=0, min_face_size=20,thresholds=[0.6, 0.7, 0
 def preprocess_image(image_path, mtcnn, OUTPUT_DIR):
     image = Image.open(image_path)
     face = mtcnn(image)
-    face = face / face.max()
     if face is not None:
+        face = face / face.max()
         face = ToPILImage()(face)
         output_path = os.path.join(OUTPUT_DIR, os.path.basename(image_path))
         face.save(output_path)
@@ -26,11 +26,11 @@ def preprocess_image_directory_and_save_output(input_dir:str, output_dir:str, su
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     image_paths = glob.glob(os.path.join(input_dir, '*'))
-    sampled_image_paths = random.sample(image_paths, min(len(image_paths), subset))
-    for i, image_path in enumerate(image_paths[:subset]):
+    image_paths = random.sample(image_paths, min(len(image_paths), subset))
+    for i, image_path in enumerate(image_paths):
         img_label = f"{output_dir}/{image_path.split('\\')[-1]}"
         # print(f"\r{i+1:03}/{subset} | {image_path:>66.66}...", end='')
-        print(f"\r{i+1:03}/{subset} | {img_label:<40.40} | {((i+1)/subset*100):.2f}%", end='')
+        print(f"\r{i+1:03}/{subset} | {img_label:<46.46} | {((i+1)/subset*100):.2f}%", end='')
         preprocess_image(image_path, mtcnn, output_dir)        
 
 remaining_dirs = [
@@ -43,12 +43,7 @@ remaining_dirs = [
 ]
 
 for INPUT_DIR, OUTPUT_DIR, ARB_LIMIT in remaining_dirs:
-    preprocess_image_directory_and_save_output(INPUT_DIR, OUTPUT_DIR, subset=5)
-
-#     if not os.path.exists(OUTPUT_DIR):
-#         os.makedirs(OUTPUT_DIR)
-#     image_paths = glob.glob(os.path.join(INPUT_DIR, '*'))
-#     sampled_image_paths = random.sample(image_paths, min(len(image_paths), ARB_LIMIT))
-#     for i, image_path in enumerate(image_paths[:ARB_LIMIT]):
-#         print(f"\r{i+1:03}/{ARB_LIMIT} | {image_path:>66.66}...", end='')
-#         preprocess_image(image_path, mtcnn, OUTPUT_DIR)        
+    batch_label = OUTPUT_DIR.split('processed')[-1]
+    print(f'Running {batch_label}:')
+    preprocess_image_directory_and_save_output(INPUT_DIR, OUTPUT_DIR, subset=ARB_LIMIT)
+    print(f'\nFinished.')
